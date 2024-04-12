8f2810079d4ed1c418b0da176f1da43924ca9aea
gbragah <gi.braga24@hotmail.com>
2024-04-12 19:42:20 -0300

    MenuCommit

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import styles from './style.css';
 import Logo from "../img/logo.png"
- 
 
-export default function Cabecalho(){
-    
 
-    return(
+export default function Cabecalho() {
+
+
+    return (
         <header>
             <figure>
-                <img src ={Logo} alt="Logo" />
+                <img src={Logo} alt="Logo" />
             </figure>
             <p className='texto'>Categorias</p>
             <input type="text" placeholder="Pesquise oque deseje encontrar" />
-            <p className="texto">Seja um colaborador</p>
-            <div className="botoes-login">
-                <button className="btn">Fazer login</button>
-                <button className="btn">Cadastre-se</button>
+                <p className="texto">Seja um colaborador</p>
+                <div className="botoes-login">
+                    <button className="btn">Fazer login</button>
+                    <button className="btn">Cadastre-se</button>
+
             </div>
         </header>
     )
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -4,14 +4,22 @@ import styles from "./style.css"
 
 export default function Menu() {
     return (
-        <nav>
-            <ul>
-                <li><a href="Home">Home</a></li>
-                <li><a href="Cursos">Cursos</a></li>
-                <li><a href="QuemSomos">Quem Somos</a></li>
-                <li><a href="Contato">Contato</a></li>
-            </ul>
-        </nav>
-
+        <div className="barralateral">
+            <button className="botaonav">
+                <div className='botao-hamburguer'>
+                    <div className='line1'></div>
+                    <div className='line2'></div>
+                    <div className='line3'></div>
+                </div>
+            </button>
+            <nav>
+                <ul>
+                    <li><a href="Home">Home</a></li>
+                    <li><a href="Cursos">Cursos</a></li>
+                    <li><a href="QuemSomos">Quem Somos</a></li>
+                    <li><a href="Contato">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
