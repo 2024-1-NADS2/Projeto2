2c91f642d3e94353c3cf36d26a0518574c18638b
gbragah <gi.braga24@hotmail.com>
2024-04-13 19:54:38 -0300

    Atualização Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,25 +1,42 @@
 import React from "react";
 import styles from "./style.css"
-
+import { BsList, BsHouse, BsBook, BsPeople, BsTelephone } from "react-icons/bs";
 
 export default function Menu() {
     return (
-        <div className="barralateral">
-            <button className="botaonav">
-                <div className='botao-hamburguer'>
-                    <div className='line1'></div>
-                    <div className='line2'></div>
-                    <div className='line3'></div>
-                </div>
-            </button>
-            <nav>
-                <ul>
-                    <li><a href="Home">Home</a></li>
-                    <li><a href="Cursos">Cursos</a></li>
-                    <li><a href="QuemSomos">Quem Somos</a></li>
-                    <li><a href="Contato">Contato</a></li>
-                </ul>
-            </nav>
-        </div>
+        <nav className="menu-lateral">
+
+            <div className="btn-expandir">
+                <BsList></BsList>
+            </div>
+
+            <ul>
+                <li className="item-menu">
+                    <a href="#">
+                        <span className="icon"><BsHouse/></span>
+                        <span className="txt-link">Home</span>
+                    </a>
+                </li>
+                <li className="item-menu">
+                    <a href="#">
+                        <span className="icon"><BsBook /></span>
+                        <span className="txt-link">Cursos</span>
+                    </a>
+                </li>
+                <li className="item-menu">
+                    <a href="#">
+                        <span className="icon"><BsPeople /></span>
+                        <span className="txt-link">Sobre Nós</span>
+                    </a>
+                </li>
+                <li className="item-menu">
+                    <a href="#">
+                        <span className="icon"><BsTelephone/></span>
+                        <span className="txt-link">Contato</span>
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
     )
 }
