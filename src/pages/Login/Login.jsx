83526c89bc72e5d6f850e99c481175743706e4fc
clicaro <149541186+clicaro@users.noreply.github.com>
2024-04-30 21:20:50 -0300

    Add files via upload

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,8 +4,37 @@ import style from './style.css';
 
 
 
+
 export default function Login(){
     return(
-        <></>
+        <body>
+        <div class="logincontainer">
+            <div class="ladocplogin">
+                <img src="logo.png" class="logologin" alt="logo"/>
+                <h1>venha aprender</h1>
+            </div>
+            <form id="login-form">
+                <div class="campologin">
+                    <h2>LOGIN</h2>
+                <div class="inputs">
+                    <label for="username">username</label>
+                    <input type="text" id="username" name="username"
+                        required/>
+                </div>
+                <div class="inputs">
+                    <label for="passaword">passaword</label>
+                    <input type="password" id="passaword" name="passaword" required/>
+                </div>
+                <div class="esqueceupassword">
+                    <label for="esqueceupassaword"></label>
+                    <a href="#">lembre me</a>
+                </div>
+                <button type="submit">LOGIN</button>
+            </div>
+            </form>
+        </div>
+        <script srg="script.js"></script>
+    
+    </body> 
     )
 }
