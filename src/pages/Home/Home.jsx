f670cb22e950ea35c370188abaf63f4e5679c092
Giovanne Braga Costa <giovanne.costa@enind.local>
2024-05-17 17:22:33 -0300

    add content home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,40 @@ export default function Home() {
             <Cabecalho />
             <Menu />
             <Conteudo>
-                <h1>Essa pagina sera a Home</h1>
+                <main>
+                    <section className='home-content'>
+                        <div className='home-content-welcome'>
+                            <div className='home-content-welcome-small'>
+                                <div className='home-content-welcome-small-show'>
+                                    <div className='home-content-welcome-small-show-msg'>
+                                        <p>Olá seja Bem-Vindo a EducaFacil!
+                                            <br />
+                                            Confira algumas de nossas tecnologias...
+                                        </p>
+                                    </div>
+                                    <div className='home-content-welcome-small-show-schools'>
+                                    <button type="button" className="front-end">Front-end</button>
+                                    <button type="button" className="front-end">Back-end</button>
+                                    <button type="button" className="front-end">Banco de Dados</button>
+                                    <button type="button" className="front-end">Programação Orientada a Objeto</button>
+                                    <button type="button" className="front-end">HTML</button>
+                                    <button type="button" className="front-end">C#</button>
+                                    <button type="button" className="front-end">Figma</button>
+                                    <button type="button" className="front-end">React</button>
+                                    <button type="button" className="front-end">Css</button>
+                                    <button type="button" className="front-end">JavaScript</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='home-content-welcome2'>
+                            <h1>Em qual tecnologia você deseja mergulhar ?</h1>
+                            <div className='home-content-welcome2-text'>
+                                Explore e encontre a organização perfeita para o seu aprendizado!
+                            </div>
+                        </div>
+                    </section>
+                </main>
             </Conteudo>
         </>
 
