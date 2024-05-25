829b1ccf2673661da3761659e547af17cd1cb3f5
Giovanne Braga Costa <gi.braga24@hotmail.com>
2024-05-25 00:37:52 -0300

    adiciona baseurl por ambiente

--- Frontend/src/pages/CursoPage/CursoPage.jsx
+++ Frontend/src/pages/CursoPage/CursoPage.jsx
@@ -1,36 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from '../CursoPage/style.css'
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
 import Menu from "../../components/Menu/Menu";
 import Conteudo from "../../components/Conteudo/Conteudo";
+import { useParams } from "react-router-dom";
+import { obterCurso } from "../../services/cursos";
 
 
 
 export default function CursoPage() {
+    const { id } = useParams()
+    const [curso, setCurso] = useState()
+
+    useEffect(() => {
+        obterCurso(id).then(setCurso)
+    })
+
     return (
         <>
-
             <Cabecalho />
             <div className='content-flex'>
                 <Menu />
                 <Conteudo>
                     <section className="videos">
+                        { curso?.videos.map((video) => {
+
+                            return(
+                                <div className="card-aula">
+                                    <div className="video-aula">
+                                        <iframe width="300" height="175" src="https://www.youtube.com/embed/1bEbBkWc4-I?si=pTB3MMVzJ5r-cPDY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                    </div>
+                                    <div className="info-aula">
+                                        <div className="titulo-aula">
+                                            <h2>Aula 01</h2>
+                                        </div>
+                                        <div>
+                                            Descrição: Introduzir os conceitos básicos do React e configurar o ambiente de desenvolvimento.
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                        
                         <div className="card-aula">
                             <div className="video-aula">
-                                <iframe width="300" height="175" src="https://www.youtube.com/embed/1bEbBkWc4-I?si=pTB3MMVzJ5r-cPDY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                            </div>
-                            <div className="info-aula">
-                                <div className="titulo-aula">
-                                    <h2>Aula 01</h2>
-                                </div>
-                                <div>
-                                    Descrição: Introduzir os conceitos básicos do React e configurar o ambiente de desenvolvimento.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card-aula">
-                            <div className="video-aula">
-                            <iframe width="300" height="175" src="https://www.youtube.com/embed/OkMhKAlEObw?si=N3zLJ8ovEp0X8RtJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                <iframe width="300" height="175" src="https://www.youtube.com/embed/OkMhKAlEObw?si=N3zLJ8ovEp0X8RtJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                             </div>
                             <div className="info-aula">
                                 <div className="titulo-aula">
@@ -43,7 +57,7 @@ export default function CursoPage() {
                         </div>
                         <div className="card-aula">
                             <div className="video-aula">
-                            <iframe width="300" height="175" src="https://www.youtube.com/embed/flqBnYMKiJE?si=295jJC3v2-vVeG0T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                <iframe width="300" height="175" src="https://www.youtube.com/embed/flqBnYMKiJE?si=295jJC3v2-vVeG0T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                             </div>
                             <div className="info-aula">
                                 <div className="titulo-aula">
@@ -55,8 +69,6 @@ export default function CursoPage() {
                             </div>
                         </div>
                     </section>
-
-
                 </Conteudo>
             </div>
 
