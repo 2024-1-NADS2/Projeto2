99ef813a69656b70d89133c0e7a3e1bcc18f9650
Giovanne Braga Costa <gi.braga24@hotmail.com>
2024-05-25 02:09:09 -0300

    integra pagina de curso

--- Frontend/src/pages/CursoPage/CursoPage.jsx
+++ Frontend/src/pages/CursoPage/CursoPage.jsx
@@ -13,8 +13,8 @@ export default function CursoPage() {
     const [curso, setCurso] = useState()
 
     useEffect(() => {
-        obterCurso(id).then(setCurso)
-    })
+        obterCurso(id).then(c => console.log(c)||c).then(setCurso)
+    }, [])
 
     return (
         <>
@@ -28,46 +28,19 @@ export default function CursoPage() {
                             return(
                                 <div className="card-aula">
                                     <div className="video-aula">
-                                        <iframe width="300" height="175" src="https://www.youtube.com/embed/1bEbBkWc4-I?si=pTB3MMVzJ5r-cPDY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                                        <iframe width="300" height="175" src={video.url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
                                     </div>
                                     <div className="info-aula">
                                         <div className="titulo-aula">
-                                            <h2>Aula 01</h2>
+                                            <h2>{video.titulo}</h2>
                                         </div>
                                         <div>
-                                            Descrição: Introduzir os conceitos básicos do React e configurar o ambiente de desenvolvimento.
+                                            {video.descricao}
                                         </div>
                                     </div>
                                 </div>
                             )
                         })}
-                        
-                        <div className="card-aula">
-                            <div className="video-aula">
-                                <iframe width="300" height="175" src="https://www.youtube.com/embed/OkMhKAlEObw?si=N3zLJ8ovEp0X8RtJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                            </div>
-                            <div className="info-aula">
-                                <div className="titulo-aula">
-                                    <h2>Aula 02</h2>
-                                </div>
-                                <div>
-                                    Descrição: Introduzir os conceitos básicos do React e configurar o ambiente de desenvolvimento.
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card-aula">
-                            <div className="video-aula">
-                                <iframe width="300" height="175" src="https://www.youtube.com/embed/flqBnYMKiJE?si=295jJC3v2-vVeG0T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                            </div>
-                            <div className="info-aula">
-                                <div className="titulo-aula">
-                                    <h2>Aula 03</h2>
-                                </div>
-                                <div>
-                                    Descrição: Introduzir os conceitos básicos do React e configurar o ambiente de desenvolvimento.
-                                </div>
-                            </div>
-                        </div>
                     </section>
                 </Conteudo>
             </div>
